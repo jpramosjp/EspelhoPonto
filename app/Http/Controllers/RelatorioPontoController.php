afff6744a033c511500c1de8f5e657b9696cd335
Joao Ramos <jpramosjp@hotmail.com>
2025-02-09 23:58:11 -0300

    Implementacao de criptografia com os cpf

--- app/Http/Controllers/RelatorioPontoController.php
+++ app/Http/Controllers/RelatorioPontoController.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Crypt;
 use Maatwebsite\Excel\Facades\Excel;
 
 class RelatorioPontoController extends Controller
@@ -16,8 +17,6 @@ class RelatorioPontoController extends Controller
     public function tela() {
         return view("tela_relatorio");
     }
-
-
 
     public function gerarRelatorio(Request $request) {
 
@@ -29,34 +28,47 @@ class RelatorioPontoController extends Controller
             'file.mimes' => 'O arquivo deve ser um dos seguintes tipos: txt',
             'file.max' => 'O arquivo não pode ser maior que 2 MB.',
         ]);
+
         $arquivoRequisicao = $request->file('file');
         $arquivo = file($arquivoRequisicao);
 
         unset($arquivo[0]);
+
+        // Carregar lista de funcionários do JSON
         $listaFuncionarios = Storage::json('de_para.json');
-        foreach($arquivo as $key => $valor) {
+
+        // Descriptografar CPFs no JSON para facilitar a busca
+        $funcionariosDescriptografados = [];
+        foreach ($listaFuncionarios["funcionarios"] as $cpfCriptografado => $nome) {
+            try {
+                $cpf = Crypt::decrypt($cpfCriptografado);
+                $funcionariosDescriptografados[$cpf] = $nome;
+            } catch (\Exception $e) {
+                Log::error("Erro ao descriptografar CPF: " . $e->getMessage());
+            }
+        }
+
+        foreach ($arquivo as $key => $valor) {
 
             $tipoLinha = substr($valor, 9, 1);
-            if($tipoLinha != 3) {
+            if ($tipoLinha != 3) {
                 continue;
             }
 
             $dadosTratados = unpack(EstruturaArquivo::estruturaTipo3(), $valor);
 
-
-
-            if(!empty($listaFuncionarios["funcionarios"][$dadosTratados["funcionario"]])){
-                $dadosTratados["funcionario"] = $listaFuncionarios["funcionarios"][$dadosTratados["funcionario"]];
+            // Buscar o nome do funcionário na lista descriptografada
+            if (!empty($funcionariosDescriptografados[$dadosTratados["funcionario"]])) {
+                $dadosTratados["funcionario"] = $funcionariosDescriptografados[$dadosTratados["funcionario"]];
             }
 
-            
             $dataHora = Carbon::parse($dadosTratados["data_hora"]);
-
             $data = $dataHora->format("d/m/Y"); 
             $hora = $dataHora->format("H:i:s"); 
             
             $dadosRelatorios[$dadosTratados["funcionario"]][$data][] = $hora;
         }
+
         return Excel::download(new ExportarExcel($dadosRelatorios), 'Relatorio de Pontos ' . Carbon::now()->format("d_m_Y") . '.xlsx');
     }
 }
